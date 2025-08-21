c30dbf0d3933c7ec24226bc9c44b33acedcb469a
achyutkneupane <achyutkneupane@gmail.com>
2025-08-21 23:38:35 +0545

    feat: adds Button component with animations

--- resources/js/Types/Inputs.ts
+++ resources/js/Types/Inputs.ts
@@ -1,8 +1,20 @@
-import { ComponentProps } from "react";
+import type { ButtonHTMLAttributes, ComponentProps, ReactNode } from "react";
 
 export interface InputFieldProps extends ComponentProps<"input"> {
     label?: string;
     errorMessage?: string;
     name: string;
     helperText?: string;
+}
+
+export interface ButtonProps
+    extends Omit<
+        ButtonHTMLAttributes<HTMLButtonElement>,
+        "className" | "onDrag" | "onDragStart" | "onDragEnd" | "onAnimationStart" | "onAnimationEnd"
+    > {
+    className?: string;
+    children: ReactNode;
+    loading: boolean;
+    isSuccess?: boolean;
+    isError?: boolean;
 }
