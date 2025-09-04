def51f8a45b125288394ccb8fbe50f4762995f39
achyutkneupane <achyutkneupane@gmail.com>
2025-09-04 19:06:43 +0545

    feat: Service provider setup added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Override;
 
@@ -25,5 +27,11 @@ final class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        Model::preventLazyLoading(! app()->isProduction());
+
+        JsonResource::withoutWrapping();
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
