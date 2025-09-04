edebd677827bbea42c66facd6813e225302452dd
achyutkneupane <achyutkneupane@gmail.com>
2025-09-04 16:50:05 +0545

    build: ssr added in vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/App.tsx"],
+            ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
